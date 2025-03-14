f1aa766b3009967b2706418b5ef79e79f7f61997
Jhuliana1403 <promiselie013@gmail.com>
2025-03-14 14:13:31 -0300

    aprendendo

--- Projeto Web/base/static/js/delay_error.js
+++
@@ -1,7 +0,0 @@
-setTimeout(function() {
-    var messageBox = document.querySelector(".messages");
-    if (messageBox) {
-        messageBox.style.opacity = "0";
-        setTimeout(() => messageBox.remove(), 500);
-    }
-}, 3000);
